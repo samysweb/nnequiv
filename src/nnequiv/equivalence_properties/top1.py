48e7b4a7e2bbddc805f59d7348233fc11e567eb2
Samuel Teuber <samuel@samweb.org>
2021-06-05 20:06:02 +0200

    Repaired top1 and introduced smaller property for experiment

--- src/nnequiv/equivalence_properties/top1.py
+++ src/nnequiv/equivalence_properties/top1.py
@@ -2,43 +2,50 @@ import numpy as np
 
 from nnenum.lpinstance import LpInstance
 from nnenum.timerutil import Timers
-from nnenum.zonotope import Zonotope
 from .property import EquivalenceProperty
 from ..zono_state import ZonoState
 
 
 class Top1Equivalence(EquivalenceProperty):
 
-	def check(self, zono : ZonoState):
+	def check(self, zono: ZonoState):
 		Timers.tic('check_top1_fallback')
-		mat0 = zono.output_zonos[0].mat_t
-		mat1 = zono.output_zonos[1].mat_t
-		bias0 = zono.output_zonos[0].center
-		bias1 = zono.output_zonos[1].center
+		output_zonos = zono.get_output_zonos()
+		mat0 = output_zonos[0].mat_t
+		mat1 = output_zonos[1].mat_t
+		bias0 = output_zonos[0].center
+		bias1 = output_zonos[1].center
+		lp_col_num = zono.lpi.get_num_cols()
 		for j in range(mat0.shape[0]):
 			lp = LpInstance(other_lpi=zono.lpi)
-			print(f"Trying {j}: ",end="")
+			#print(f"Trying {j}: ", end="")
 			current_bias0 = bias0 - bias0[j]
 			current_mat0 = mat0 - mat0[j]
 			for k in range(mat0.shape[0]):
-				if k==j:
+				if k == j:
 					continue
-				lp.add_dense_row(current_mat0[k],-current_bias0[k])
+				lp_row = current_mat0[k, :lp_col_num]
+				alpha_row = current_mat0[k, lp_col_num:]
+				ib = np.array(output_zonos[-1].init_bounds, dtype=output_zonos[-1].dtype)
+				alpha_min = lp.compute_residual(alpha_row, ib[lp_col_num:])
+				lp.add_dense_row(lp_row, -current_bias0[k] - alpha_min)
 			if not lp.is_feasible():
-				print(f"Skipped")
+				# print(f"Skipped")
 				continue
-			else:
-				print(f"Checking...")
+			#else:
+			#	print(f"Checking...")
 			current_bias1 = bias1 - bias1[j]
 			current_mat1 = mat1 - mat1[j]
 			for k in range(mat1.shape[0]):
 				# if k==j:
 				# 	continue
-				max_vec =lp.minimize(-current_mat1[k])
-				max_val = current_bias1[k]+np.dot(current_mat1[k],max_vec)
-				if max_val>0:
+				max_vec = lp.minimize(-current_mat1[k, :lp_col_num])
+				max_val = self.compute_deviation(max_vec, current_bias1[k], current_mat1[k], output_zonos[-1].init_bounds, 1,
+				                                 output_zonos[-1].dtype)
+				#max_val = current_bias1[k] + np.dot(current_mat1[k], max_vec)
+				if max_val > 0:
 					Timers.toc('check_top1_fallback')
-					return False,(k,max_vec)
+					return False, (k, max_vec)
 
 		Timers.toc('check_top1_fallback')
 		return True, (None, None)
@@ -50,4 +57,4 @@ class Top1Equivalence(EquivalenceProperty):
 		return False
 
 	def check_out(self, r1, r2):
-		return np.argmax(r1)==np.argmax(r2)
+		return np.argmax(r1) == np.argmax(r2)
